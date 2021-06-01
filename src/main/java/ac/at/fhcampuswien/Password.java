0a6ae71340bb44f11265fe316c9c0754a2b6b682
fakhreddine kenbi <71633029+kenbi12@users.noreply.github.com>
2021-06-01 21:06:07 +0200

    first commit

---
+++ src/main/java/ac/at/fhcampuswien/Password.java
@@ -0,0 +1,14 @@
+package ac.at.fhcampuswien;//mit Imran Kambal gelöst!!!
+
+public class App {
+    String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
